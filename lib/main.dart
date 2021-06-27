9ae7169d2986a4a2aef375bd6794d6964f14af19
Ferhat Sayan <fs@nitash.dev>
2021-06-27 12:03:53 +0200

    added first splash logic to load all data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,12 +8,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rick and Morty Explorer',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
+    return GetMaterialApp(
+        title: 'Rick and Morty Explorer',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/splash',
+        getPages: [],
+        builder: (context, child) {
+          return BlocListener<DataCubit, DataState>(listener: (context, state) {
+            switch (state.type) {
+              case DataType.LOADING:
+                EasyDebounce.debounce(
+                  'debounce-data',
+                  Duration(milliseconds: 500),
+                  () async => await Get.offAllNamed('/splash'),
+                );
+            }
+          });
+        });
   }
 }
